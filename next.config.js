6714aff5d66f215b87bc5b1e68818123ec5ffd2a
Alex K.A. <alexakustov@gmail.com>
2025-07-22 19:42:05 +0100

    Add security headers and sanitize script loading

--- next.config.js
+++ next.config.js
@@ -1,3 +1,42 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://wallet.axate.io https://wallet-staging.axate.io;
+  child-src 'none';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' https://source.unsplash.com https://images.unsplash.com data:;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
 module.exports = {
   images: {
     remotePatterns: [
@@ -10,5 +49,13 @@ module.exports = {
   },
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
